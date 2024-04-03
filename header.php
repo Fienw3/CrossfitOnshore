cda0c4738cc103ff3928a5569d0c7cf7e91434c0
Fienw3 <annwal01@easv365.dk>
2024-04-03 10:54:18 +0200

    Basic setup for wordpress template

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html <?php language_attributes( ) ?>>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <<title><?php bloginfo("name"); ?></title>
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <?php wp_head(); ?>
+</head>
+
+<body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+<header>
+
+<!--Navbar-->
+<nav>
+
+</nav>
