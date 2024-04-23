740dff86a0e6ecaa6430b0bac0db4755c0903794
Fienw3 <annwal01@easv365.dk>
2024-04-23 14:27:22 +0200

    Sneakpeaks + Journey (About us)

--- page-about.php
+++ page-about.php
@@ -128,7 +128,45 @@
 </section>
 
 
-<section></section>
+<!-- Sneakpeaks -->
+<section>
+    <div class="container_sneakpeak d-flex justify-content-center align-items-center">
+        <div class="col-10">
+            <div style="width: 50%;">
+                <h5 class="text_blue">Holdoversigt</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..</p>
+                <button class="btn_2">SE MERE</button>
+            </div>
+            <div style="width: 50%;"></div>
+        </div>
+    </div>
+    <div class="container_sneakpeak d-flex justify-content-center align-items-center shadow">
+        <div class="col-10">
+            <div style="width: 50%;">
+            </div>
+            <div style="width: 50%;">
+                <h5 class="text_blue">KONSULTATION</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..</p>
+                <button class="btn_2">BOOK KONSULTATION</button>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- Start Journey Section -->
+<section>
+    <div class="container_end d-flex justify-content-center align-items-center">
+        <div class="col-10">
+            <div class="center text_white">
+                <h1>DIN REJSE STARTER HER</h1>
+                <p>er du klar til at starte din Crossfit rejse? Du vil som medlem i CrossFit Onshore blive mødt af instruktører som alle har det fælles mål, at gøre din træning så effektiv og målrettet som overhovedet mulig.</p>
+                <button class="btn_1">BLIV MEDLEM</button>
+            </div>
+    
+        </div>
+    </div>
+</section>
 
 </body>
 
